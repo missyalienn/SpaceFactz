9939e13af735cad93d2e06070072e925dd4462bd
missyalienn <allan.missy@gmail.com>
2017-02-15 12:35:24 -0500

    added space facts to planet tableview controller

---
+++ NASA_API/Secrets.swift
@@ -0,0 +1,22 @@
+//
+//  Secrets.swift
+//  NASA_API
+//
+//  Created by Missy Allan on 11/3/16.
+//  Copyright © 2016 Missy Allan. All rights reserved.
+//
+
+import Foundation
+
+
+class Secrets {
+    
+    
+   static let baseURL = "https://api.nasa.gov/planetary/apod?"
+    
+   static let apiKey = "api_key=6rmChDNmRMvIC1L2OiFteu6l1NNvHwuDznjdfaLI"
+    
+    
+    
+    
+}
