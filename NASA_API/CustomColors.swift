ddd2f3260818c7a25566f2954bda5a7cb88934d4
missyalienn <allan.missy@gmail.com>
2017-02-17 10:56:34 -0500

    changed gradient color

---
+++ NASA_API/CustomColors.swift
@@ -0,0 +1,20 @@
+//
+//  CustomColors.swift
+//  NASA_API
+//
+//  Created by Missy Allan on 2/17/17.
+//  Copyright © 2017 Missy Allan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+ 
+    public static let themeSunshine = UIColor(red:1.00, green:0.88, blue:0.33, alpha:1.0)
+    public static let themeCreamsicle = UIColor(red:1.00, green:0.74, blue:0.41, alpha:1.0)
+    public static let themeCoral = UIColor(red:0.95, green:0.56, blue:0.52, alpha:1.0)
+    public static let themePink =  UIColor(red:0.77, green:0.43, blue:0.62, alpha:1.0)
+    public static let themePurple = UIColor(red:0.60, green:0.40, blue:0.64, alpha:1.0)
+}
+
